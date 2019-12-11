a56e030389bf6c79faf0ca5d3b9702596773192d
fatima ahmed <fatymah001@gmail.com>
2019-12-11 17:57:02 +0300

    Fix typo in cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,12 +1,12 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origin 'http://localhost:3000'
-    resources '*' headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
+    origins 'http://localhost:3000'
+    resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
   end
 
   allow do
-    origin 'http://localhost:3000' 
+    origins 'https://conference-schedule-app.herokuapp.com/' 
     # put heroku link here
-    resources '*' headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
+    resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
   end
 end
