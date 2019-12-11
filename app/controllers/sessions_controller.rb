df5b432ad15d737867724f89b86725205c3fbc74
fatima ahmed <fatymah001@gmail.com>
2019-12-11 20:24:33 +0300

    Implement logged_out action in sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -30,4 +30,11 @@ class SessionsController < ApplicationController
     }
   end
 
+  def logged_out
+    reset_session
+    render json: {
+      status: 200,
+      logged_out: true
+    }
+  end
 end
