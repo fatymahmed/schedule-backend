33cf93d9a600d368187b983dcc157fd47124859e
fatima ahmed <fatymah001@gmail.com>
2019-12-11 21:18:00 +0300

    Update domain in session_store file

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,7 @@
 if Rails.env == "production"
-  Rails.application.config.session_store :cookie_store, key: '_authentication_app', 
-                                       domain: 'https://conference-schedule-app.herokuapp.com/'
+  Rails.application.config.session_store :cookie_store,
+                                         key: '_authentication_app',
+                                         domain: 'https://events-scheduler-api.herokuapp.com/'
 else
   Rails.application.config.session_store :cookie_store, key: '_authentication_app'
 end
