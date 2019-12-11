2f26b240b777b4399aeaa2879a0b73f2fa7a209e
fatima ahmed <fatymah001@gmail.com>
2019-12-11 20:11:31 +0300

    Implement create action for registrations controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < ApplicationController
   def create
     user = User
             .find_by(email: params['user']['email'])
-            .try(:authenticate, params['password']['password'])
+            .try(:authenticate, params['user']['password'])
     if user
       session[:user_id] = user.id
       render json: {
