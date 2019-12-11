e1f0d6a73541c66acfd63aa595026c12cde76d2c
fatima ahmed <fatymah001@gmail.com>
2019-12-11 20:23:47 +0300

    Implement logged_in action in sessions that includes a concern

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,6 @@
 class SessionsController < ApplicationController
+  include CurrentUserConcern
+
   def create
     user = User
             .find_by(email: params['user']['email'])
@@ -16,4 +18,16 @@ class SessionsController < ApplicationController
       }
     end
   end
+
+  def logged_in
+    if @current_user
+      render json: {
+        logged_in: true,
+        user: @current_user
+      }
+    else render json: {
+      logged_in: false
+    }
+  end
+
 end
