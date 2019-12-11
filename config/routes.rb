be6733775d2a58d97ad916c81e98d7d16e88a472
fatima ahmed <fatymah001@gmail.com>
2019-12-12 02:02:54 +0300

    Update logout route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :sessions, only: :create
   resources :registrations, only: :create
-  delete :logout, to: 'sessions#logout'
+  delete :logout, to: 'sessions#logged_out'
   get :logged_in, to: 'sessions#logged_in'
   root to: 'static#home'
 end
