741a1e080f776fe15f5c43bb02be1995629caa2a
fatima ahmed <fatymah001@gmail.com>
2019-12-13 18:25:46 +0300

    Update seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 10.times do |talk|
   Talk.create(title: "opening speech #{talk}",
               description: 'orem ipsum dolor sit amet, consectetur adipiscing,
@@ -8,7 +6,7 @@ Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
 aliquip ex ea commodo consequat',
               startTime: "DateTime.new(2012, 8, 29, 22, 35, 0).change(day: #{talk})",
               endTime: "DateTime.new(2012, 8, 29, 22, 35, 0).change(day: #{talk}+3)",
-              location: "hall #{talk}", speakers: ['speaker {talk}'])
+              location: "hall #{talk}", speakers: ["speaker #{talk}"])
 end
 
 puts '10 Talks created'
