3ca36ccd2de5ead89612ba3301c6f29a0ca399b3
fatima ahmed <fatymah001@gmail.com>
2019-12-18 16:51:04 +0300

    Add live link in allowed domain

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,7 @@
 if Rails.env == "production"
   Rails.application.config.session_store :cookie_store,
                                          key: '_authentication_app',
-                                         domain: 'https://events-scheduler-api.herokuapp.com/'
+                                         domain: 'events-scheduler-api.herokuapp.com/'
 else
   Rails.application.config.session_store :cookie_store, key: '_authentication_app'
 end
